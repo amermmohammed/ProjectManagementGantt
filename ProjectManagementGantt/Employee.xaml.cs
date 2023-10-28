9cbcfb00dbfd982c2bfe3803adc7209554943b29
ss-netigo <soroush.shalali@netigo.de>
2023-10-28 23:00:13 +0200

    created database

--- ProjectManagementGantt/Employee.xaml.cs
+++ ProjectManagementGantt/Employee.xaml.cs
@@ -11,17 +11,66 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SQLite;
 
 namespace ProjectManagementGantt
 {
-    /// <summary>
-    /// Interaktionslogik für Employee.xaml
-    /// </summary>
     public partial class Employee : Window
     {
         public Employee()
         {
             InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameTxt.Text;
+            string lastname = lastNameTxt.Text;
+            string dep = depTxt.Text;
+            string tel = telTxt.Text;
+
+            bool success = InsertEmployee(firstName, lastname, dep, tel);
+
+            if (success)
+            {
+                firstNameTxt.Text = string.Empty;
+                lastNameTxt.Text = string.Empty;
+                depTxt.Text = string.Empty;
+                telTxt.Text = string.Empty;
+            }
+        }
+
+        public static bool InsertEmployee(string firstName, string lastName, string department, string tel)
+        {
+            string connectionString = "Data Source=db.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+
+                string insertQuery = "INSERT INTO employees (firstname, lastname, department, tel) VALUES (@firstName, @lastName, @department, @tel);";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(insertQuery, connection))
+                {
+                    cmd.Parameters.AddWithValue("@firstName", firstName);
+                    cmd.Parameters.AddWithValue("@lastName", lastName);
+                    cmd.Parameters.AddWithValue("@department", department);
+                    cmd.Parameters.AddWithValue("@tel", tel);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    connection.Close();
+
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            EmployeesWindow employeesWindow = new EmployeesWindow();
+            employeesWindow.Show();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
--- ProjectManagementGantt/EmployeesWindow.xaml.cs
+++ ProjectManagementGantt/EmployeesWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,23 +13,104 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Runtime.Remoting.Messaging;
+using System.ComponentModel;
 
 namespace ProjectManagementGantt
 {
     /// <summary>
     /// Interaktionslogik für EmployeesWindow.xaml
     /// </summary>
+    /// 
+
+
     public partial class EmployeesWindow : Window
     {
+        private DataTable yourDataTable = new DataTable();
         public EmployeesWindow()
         {
             InitializeComponent();
+            crateDb();
+            UpdateDataTable();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Employee employee = new Employee();
             employee.Show();
+        }
+
+        public static bool crateDb()
+        {
+            string connectionString = "Data Source=db.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string createTableQuery = "CREATE TABLE IF NOT EXISTS employees (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "firstname TEXT, " +
+                    "lastname TEXT, " +
+                    "department TEXT, " +
+                "tel TEXT);";
+
+                using (SQLiteCommand cmd = new SQLiteCommand(createTableQuery, connection))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+
+                connection.Close();
+            }
+
+            return true;
+
+        }
+
+        public static DataTable GetEmployees()
+        {
+            string connectionString = "Data Source=db.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT * FROM employees;";
+                using (SQLiteCommand cmd = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(cmd))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        public void UpdateDataTable()
+        {
+            DataTable newDataTable = GetEmployees();
+
+            yourDataTable.Clear();
+            yourDataTable.Merge(newDataTable);
+
+            ICollectionView dataView = CollectionViewSource.GetDefaultView(yourDataTable);
+
+            dataGridView1.ItemsSource = dataView;
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void editEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void get_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
